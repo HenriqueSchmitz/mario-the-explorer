2697c4efa39671c9983b669f858844f09d35a1c8
Henrique Schmitz <schhenrique@gmail.com>
2026-05-04 17:10:59 -0400

    chore: expose TileType

--- src/mario_the_explorer/environment/__init__.py
+++ src/mario_the_explorer/environment/__init__.py
@@ -1,4 +1,4 @@
-from .tiles import Tile, tile_absolute_id
+from .tiles import Tile, TileType, tile_absolute_id
 from .rewards import RewardModel
 from .visualization import ScreenOverlay
 from .super_mario_env import SuperMarioWorldEmulator
