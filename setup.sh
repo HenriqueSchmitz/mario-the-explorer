c160eaff0aaab0f66d8a28b10ec02ca6dc25fa4b
Henrique Schmitz <schhenrique@gmail.com>
2026-05-08 17:30:49 -0400

    feat: game state with yellow button clicked

--- setup.sh
+++ setup.sh
@@ -6,3 +6,4 @@ cd "$parent_path" || exit 1
 
 pip install --no-cache-dir -q .
 python -m retro.import ./roms
+cp ./states/* "/usr/local/lib/python3.12/dist-packages/stable_retro/data/stable/SuperMarioWorld-Snes-v0/"
